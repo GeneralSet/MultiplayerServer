107be73c7bcc6a141b8679f4dad922fe589e6d98
Tim <hello@timbest.net>
2018-05-06 04:33:26 -0700

    add all colors

--- client/src/deckBuilder/allTheDecks.tsx
+++ client/src/deckBuilder/allTheDecks.tsx
@@ -16,10 +16,21 @@ function getRand<T>(options: T[]): T[] {
   return selected;
 }
 
+function getRandColors(): string[] {
+  const colors: string[] = [];
+  while (colors.length < OPTIONS) {
+    const color = '#' + (Math.random()*0xFFFFFF<<0).toString(16);
+    if (!colors.includes(color)) {
+      colors.push(color);
+    }
+  }
+  return colors;
+}
+
 const validNumbers = [1, 2, 3, 4, 5];
 const validShapes = Object.values(shapes);
 const validShaddings = Object.values(patterns);
-const validColors = ['#ED254E', '#F9DC5C', '#011936'];
+const validColors = getRandColors();
 
 for (let i = 0; i < NUM_DECKS; i++) {
   const DECK_DATA = {
