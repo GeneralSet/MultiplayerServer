28ee0219bf4500aeed710ab10ce6c4fd8edac181
Tim <hello@timbest.net>
2018-05-06 03:52:50 -0700

    working all the decks builder

--- client/src/deckBuilder/allTheDecks.tsx
+++ client/src/deckBuilder/allTheDecks.tsx
@@ -4,38 +4,54 @@ import { shapes } from './features/shapes';
 
 const NUM_DECKS = 1;
 
+function shuffle(array: any): any {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function getRand<T>(options: T[]): T[] {
   const selected: T[] = [];
-  let index = Math.floor(Math.random() * options.length);
-  selected.push(options[index]);
-  options = options.splice(index, 1);
+  // let index = Math.floor(Math.random() * options.length);
+  selected.push(options[0]);
+  // const options1 = options.splice(0, 1);
 
-  index = Math.floor(Math.random() * options.length);
-  selected.push(options[index]);
-  options = options.splice(index, 1);
+  // const index1 = Math.floor(Math.random() * options1.length);
+  selected.push(options[1]);
+  // const options2 = options1.splice(1, 1);
 
-  index = Math.floor(Math.random() * options.length);
-  selected.push(options[index]);
+  // const index3 = Math.floor(Math.random() * options.length);
+  selected.push(options[2]);
 
   return selected;
 }
 
 const validNumbers = [1, 2, 3, 4, 5];
-// const validShapes = Object.keys(shapes);
-// const validShaddings = Object.keys(patterns);
+const validShapes = Object.values(shapes);
+const validShaddings = Object.values(patterns);
 const validColors = ['#ED254E', '#F9DC5C', '#011936'];
 
 for (let i = 0; i < NUM_DECKS; i++) {
   const DECK_DATA = {
-    // shapes: getRand(validShapes),
-    shapes: [shapes.rightSideUp, shapes.right, shapes.upsideDown],
-    colors: getRand(validColors),
-    shadings: [patterns.open, patterns.striped, patterns.triangles],
-
-    // shadings: getRand(validShaddings),
-    numbers: getRand(validNumbers),
+    shapes: getRand(shuffle(validShapes)),
+    colors: getRand(shuffle(validColors)),
+    shadings: getRand(shuffle(validShaddings)),
+    numbers: getRand(shuffle(validNumbers)),
   };
 
-  const generator = new GeometricDeckGenerator(DECK_DATA );
+  const generator = new GeometricDeckGenerator(DECK_DATA as any);
   generator.exportDeck(`./decks/${i}/`);
 }
